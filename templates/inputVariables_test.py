b1697373f94187dd8e5e743483363454a4b52f12
Bonello, Josh <josh.bonello@egov.com>
2017-06-21 14:41:19 -0500

    First 3 tests functioning. Refactored source code to be able to test.

--- templates/inputVariables_test.py
+++ templates/inputVariables_test.py
@@ -11,10 +11,24 @@ class InputVariablesTestCase(unittest.TestCase):
         result =  inputVariables.main(['-i', 'infile'])
         self.assertEqual(result, "inputfile argument is : infile\noutputfile argument is : ")
 
+    def test_infile_longoptions(self):
+        result =  inputVariables.main(['--ifile', 'infile'])
+        self.assertEqual(result, "inputfile argument is : infile\noutputfile argument is : ")
+
     def test_outfile(self):
         result = inputVariables.main(['-o', 'outfile'])
         self.assertEqual(result, "inputfile argument is : \noutputfile argument is : outfile")
 
-    def test_longoptions(self):
+    def test_outfile_longoptions(self):
+        result = inputVariables.main(['--ofile', 'outfile'])
+        self.assertEqual(result, "inputfile argument is : \noutputfile argument is : outfile")
+
+    def test_inandoutfile(self):
+        result = inputVariables.main(['-i','infile','-o','outfile'])
+        self.assertEqual(result, "inputfile argument is : infile\noutputfile argument is : outfile")
+
+    def test_inandoutfile_longoptions(self):
+        result = inputVariables.main(['--ifile','infile','--ofile','outfile'])
+        self.assertEqual(result, "inputfile argument is : infile\noutputfile argument is : outfile")
 if __name__ == '___main___':
     unittest.main()
